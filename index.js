df1b4e812d691c5f3d8d7ce8e66677fa05d9be5e
Sejal Sinha <sejalsinha@codeyourself.org>
2025-02-13 12:19:38 +0530

    Added display text

--- index.js
+++ index.js
@@ -15,3 +15,14 @@ var randomImageSource2 =  "images/dice" + randomNumber2 + ".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
+// Determine the winner and display the result
+var resultText = "";
+if (randomNumber1 > randomNumber2) {
+    resultText = "Player 1 Wins!";
+} else if (randomNumber1 < randomNumber2) {
+    resultText = "Player 2 Wins!";
+} else {
+    resultText = "It's a Draw!";
+}
+
+document.querySelector("h1").textContent = resultText;
